03b1c07b5eee99515226909dd4d266fc8abbe84e
笑男 常 <1481167030@qq.com>
2022-01-14 22:16:54 +0800

    Initial commit

---
+++ src/test/java/com/chat/graduated_design/GraduatedDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.graduated_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduatedDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
