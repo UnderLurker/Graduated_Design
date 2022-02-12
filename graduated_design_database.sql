1d67da103a3d0baca00e52299e5be9e5549fe58b
笑男 常 <1481167030@qq.com>
2022-02-12 17:49:11 +0800

    新增两个表结构 2022/2/12

--- graduated_design_database.sql
+++ graduated_design_database.sql
@@ -11,11 +11,40 @@
  Target Server Version : 80024
  File Encoding         : 65001
 
- Date: 07/02/2022 15:55:30
+ Date: 12/02/2022 17:48:40
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for contact
+-- ----------------------------
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE `contact`  (
+  `no` int(0) NOT NULL COMMENT '无意义，主键',
+  `userid` int(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `contactid` int(0) NOT NULL COMMENT '联系人id',
+  PRIMARY KEY (`no`) USING BTREE,
+  INDEX `userid`(`userid`) USING BTREE,
+  CONSTRAINT `contact_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for file_storage
+-- ----------------------------
+DROP TABLE IF EXISTS `file_storage`;
+CREATE TABLE `file_storage`  (
+  `Id` int(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '发送者的id',
+  `uuid` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '唯一的id',
+  `originname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '文件的原名',
+  `datetime` datetime(0) NOT NULL COMMENT '文件存储时间',
+  `type` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '文件类型',
+  `folder` int(0) NOT NULL COMMENT '所属文件夹',
+  PRIMARY KEY (`uuid`) USING BTREE,
+  INDEX `Id`(`Id`) USING BTREE,
+  CONSTRAINT `file_storage_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user
@@ -28,10 +57,9 @@ CREATE TABLE `user`  (
   `gender` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '性别',
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱',
   `phone` char(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '电话号码',
-  `contact` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '联系人列表',
   `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '昵称',
-  `headportrait` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '头像地址',
+  `active` tinyint(1) NOT NULL DEFAULT 0 COMMENT '账户是否激活',
   PRIMARY KEY (`Id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
