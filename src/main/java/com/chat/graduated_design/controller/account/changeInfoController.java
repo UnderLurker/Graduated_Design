b0e9924beae168397d1847832dcbd5336c9a4b50
笑男 常 <1481167030@qq.com>
2022-02-12 16:22:29 +0800

    更新目录结构 2022/2/12

--- src/main/java/com/chat/graduated_design/controller/account/changeInfoController.java
+++ src/main/java/com/chat/graduated_design/controller/account/changeInfoController.java
@@ -1,10 +1,10 @@
-package com.chat.graduated_design.controller;
+package com.chat.graduated_design.controller.account;
 
-import com.chat.graduated_design.entity.Response;
-import com.chat.graduated_design.entity.User;
-import org.springframework.stereotype.Controller;
+import com.chat.graduated_design.entity.user.User;
+import com.chat.graduated_design.message.Response;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,15 +12,14 @@ import javax.servlet.http.HttpSession;
 
 /**
  * @program: Graduated_Design
- * @description: 修改信息控制器
+ * @description: 用户修改信息控制类
  * @author: 常笑男
- * @create: 2022-02-07 09:40
+ * @create: 2022-02-12 16:13
  **/
-@Controller
-public class changeInfo {
-    @ResponseBody
+@RestController
+public class changeInfoController {
     @RequestMapping(value = "/changeInfo")
-    public Response changeInfo(HttpServletRequest request){
+    public Response changeInfo(@RequestBody User user, HttpServletRequest request){
         ModelAndView mav=new ModelAndView();
         HttpSession session=request.getSession();
         User saveUser=(User) session.getAttribute("user");
