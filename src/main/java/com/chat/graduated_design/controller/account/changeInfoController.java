90af1ce547bcdd63775b32d2efaa4490e5940af1
笑男 常 <1481167030@qq.com>
2022-02-16 17:28:36 +0800

    用户修改自己的信息 2022/2/12

--- src/main/java/com/chat/graduated_design/controller/account/changeInfoController.java
+++ src/main/java/com/chat/graduated_design/controller/account/changeInfoController.java
@@ -1,14 +1,21 @@
 package com.chat.graduated_design.controller.account;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.chat.graduated_design.entity.user.User;
 import com.chat.graduated_design.message.Response;
+import com.chat.graduated_design.service.impl.userServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @program: Graduated_Design
@@ -18,13 +25,60 @@ import javax.servlet.http.HttpSession;
  **/
 @RestController
 public class changeInfoController {
-    @RequestMapping(value = "/changeInfo")
-    public Response changeInfo(@RequestBody User user, HttpServletRequest request){
+    @Autowired
+    private userServiceImpl userService;
+
+    @PostMapping(value = "/changeInfo")
+    public Response changeInfo(@RequestBody User user,HttpSession session){
         ModelAndView mav=new ModelAndView();
-        HttpSession session=request.getSession();
-        User saveUser=(User) session.getAttribute("user");
-//        saveUser.setName(name);
-//        saveUser.setNickname(nickname);
-        return Response.ok("/changeInfo");
+        User clientUser=(User)session.getAttribute("user");
+        Map<String,String> map=new HashMap<>();
+        if(clientUser.equals(user)){
+            map.put("title","修改结果");
+            map.put("msg","您未修改信息！！！");
+            return Response.error("/changeInfo",map);
+        }
+        //需要查询数据库看是否重复
+        if(!clientUser.getEmail().equals(user.getEmail())){
+            QueryWrapper<User> emailQuery=new QueryWrapper<>();
+            emailQuery.eq("email",user.getEmail());
+            List<User> emailUser=userService.list(emailQuery);
+            if(!emailUser.isEmpty()){
+                map.put("title","修改失败");
+                map.put("msg","您的新邮箱已被注册！！！");
+                map.put("email",clientUser.getEmail());
+                map.put("phone",clientUser.getPhone());
+                return Response.error("/changeInfo",map);
+            }
+        }
+        if(!clientUser.getPhone().equals(user.getPhone())){
+            QueryWrapper<User> phoneQuery=new QueryWrapper<>();
+            phoneQuery.eq("phone",user.getPhone());
+            List<User> phoneUser=userService.list(phoneQuery);
+            if(!phoneUser.isEmpty()){
+                map.put("title","修改失败");
+                map.put("msg","您的新手机号码已被注册！！！");
+                map.put("phone",clientUser.getPhone());
+                return Response.error("/changeInfo",map);
+            }
+        }
+
+        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
+        queryWrapper.eq("Id",clientUser.getId());
+        User queryUser=userService.getOne(queryWrapper);
+
+        clientUser.setName(user.getName());
+        clientUser.setNickname(user.getNickname());
+        clientUser.setGender(user.getGender());
+        clientUser.setPassword(queryUser.getPassword());
+        clientUser.setEmail(user.getEmail());
+        clientUser.setPhone(user.getPhone());
+
+
+        userService.update(clientUser,queryWrapper);
+        session.setAttribute("user",clientUser);
+        map.put("title","修改成功");
+        map.put("msg","您成功修改了自己的个人信息！！！");
+        return Response.ok("/changeInfo",map);
     }
 }
