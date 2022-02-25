8cc24a2d8664d808daad890562a69545d1071b21
笑男 常 <1481167030@qq.com>
2022-02-25 17:03:51 +0800

    结构优化并实现聊天功能 2022/2/25

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,6 @@
+
+let statisticsActive=-1;
+let websocket = null; //websocket连接服务器使用
 function sendPost(url, data, callback) {
     axios({
         url: url,
@@ -24,22 +27,47 @@ function sendGet(url, param, callback) {
     });
 }
 
+//发送消息
+function send(fromUser,toUser,cotent) {
+    //获取输入的文本信息进行发送
+    const chatMsg = {
+        content: cotent,
+        dest: toUser,
+        origin: fromUser
+    };
+
+    websocket.send(JSON.stringify(chatMsg));
+}
+
+
+function formatDate(dateString){
+    let time=new Date(dateString);
+    return {
+        year:time.getFullYear(),
+        mouth:time.getMonth()+1,
+        day:time.getDate(),
+        hour:time.getHours(),
+        minute:time.getMinutes()
+    }
+}
+
 let vue = new Vue({
     el: "#app",
     computed: {
         showTitle() {
-            return (this.messagebox.option == 0) ? this.messagebox.folderTitle : this.messagebox.title;
+            return (this.messagebox.option === 0) ? this.messagebox.folderTitle : this.messagebox.title;
         },
         showMsg() {
-            return (this.messagebox.option == 0) ? this.messagebox.folderMsg : this.messagebox.msg;
+            return (this.messagebox.option === 0) ? this.messagebox.folderMsg : this.messagebox.msg;
         },
-        // getClassify(folder){
-        //     return this.contact.filter((con)=>{
-        //         return con.folder==folder;
-        //     });
-        // }
+        userPhone(){
+            return this.userInfo.phone.slice(0,3)+' '+this.userInfo.phone.slice(3,7)+' '+this.userInfo.phone.slice(7);
+        },
     },
     data: {
+        currentChat:{
+            contactInfo: {},
+        },
         messagebox: {
             //0移除联系人分组 1其他提示信息
             option: 0,
@@ -55,14 +83,40 @@ let vue = new Vue({
             gender: '',
             email: '',
             phone: '',
+            id: -1,
+            headportrait:'',
         },
         contactClassify: [],
         searchClassify: ['用户', '文件', '图片', '视频', '音频'],
         contact:[],
-        searchResult:[[],[],[],[],[]],
+        searchResult:{
+            user:[],
+            file:[],
+            photo:[],
+            video:[],
+            music:[]
+        },
         searchContent:{
             content:''
-        }
+        },
+        selected:{
+            folderName:'',
+            selectedPerson:[]
+        },
+        addOrEdit:true,//true为添加 false为编辑
+        frame:{
+            frameActive:0,
+            first_left:36,
+        },
+        contactSelect:{
+            frameActive:-1,
+            contactActive:-1,
+        },
+        sendInfo:{
+            content:'',
+            fromUser:'',
+            toUser:'',
+        },
     },
     methods: {
         changeInfo(e) {
@@ -74,7 +128,7 @@ let vue = new Vue({
                 Vue.set(vue.messagebox, 'msg', responseInfo.msg);
                 $('.create-chat-folder').css('display', 'block');
                 $('.cancel').css('display', 'none');
-                if (response.status == 500) {
+                if (response.status === 500) {
                     if (responseInfo.email != null) {
                         $('#UserEmail').val(responseInfo.email);
                     }
@@ -93,42 +147,160 @@ let vue = new Vue({
         },
         okOption: function () {
             $('.create-chat-folder').css('display', 'none');
-            if (vue.messagebox.option == 0) {
+            if (vue.messagebox.option === 0) {
                 //提交联系人分类表单
 
             }
+            else if(vue.messagebox.option===2){
+                //提交新增联系人分类表单
+                sendPost('/folder/put/'+getCookie('id'),
+                    this.selected,
+                    (msg)=>{
+                        let response=msg.data;
+                        if(response.status===200){
+                            Vue.set(this.messagebox, 'option', -1);
+                            Vue.set(this.messagebox, 'title', "联系人分组");
+                            Vue.set(this.messagebox, 'msg', response.obj);
+                            $('#folder-operation').css({'width':'0','opacity':'0'});
+                            $('.user-setting-hidden:eq(1)').css({'width':'384px','opacity':'1'});
+                            this.contactClassify.push(this.selected.folderName);
+                            this.contact.push(this.selected.selectedPerson);
+                            $('.create-chat-folder').css('display', 'block');
+                            $('.cancel').css('display', 'none');
+                        }
+                        else{//status==500
+
+                        }
+                    });
+            }
         },
         searchSubmit(){
-            if(this.searchContent.content=='') return;
+            if(this.searchContent.content==='') return;
             sendGet('/search/'+getCookie('id'),
-                this.searchContent,
-                (msg)=>{
-                let data=msg.data.obj;
-                this.searchResult[0]=data.user;
-            });
+                this.searchContent,(msg)=>{
+                    let data=msg.data.obj;
+                    console.log(data.user);
+                    this.searchResult.user=data.user;
+                });
         },
         clearSearch(){
             this.searchContent.content="";
-        }
-    },
-    components: {
-        list: {
-            props: ['msg'],
-            template: `<div class="list">
-                        <template v-for="(classify,index) in msg" :key="index">
-                            <div class="frame">
-                    <div class="title">
-                        {{classify}}
-                    </div>
-                    <div class="unread">
-                        <ion-icon name="alert-outline"></ion-icon>
-                    </div>
-                </div>
-                        </template>
-                    </div>`,
+        },
+        addSelected(){
+            this.selected.selectedPerson.length=0;
+            for(let person of this.contact[0]){
+                if(person.select){
+                    this.selected.selectedPerson.push(person);
+                }
+            }
+
+            $('#folder-operation').css({'width':'384px','opacity':'1'});
+            $('#select-mumbers').css({'width':'0','opacity':'0'});
+        },
+        deleteContactFromFolder(){
+            // for(var i=0;i<folderItems.length;i++){
+            //     folderItems[i].onclick=function(e){
+            //         Vue.set(vue.messagebox, 'option', 0);
+            //         // addRippleEffect(e,this,"rgba(120,120,120,.25)");
+            //         let name=this.getElementsByTagName('span')[0].innerHTML;
+            //         let folderFormInput=folderForm.getElementsByTagName('input')[0];
+            //         folderFormInput.value=name;
+            //         expandFolder(this);
+            //         folderTitle.innerHTML="编辑分类";
+            //         folderSetting.innerHTML="<ion-icon name='trash-outline'></ion-icon>";
+            //     }
+            // }
+        },
+        folderSubmit(){
+            Vue.set(this.messagebox, 'option', -1);
+            Vue.set(this.messagebox, 'title', "联系人分组");
+            if(this.selected.folderName===""){
+                Vue.set(this.messagebox, 'msg', "您的分组名为空");
+                $('.create-chat-folder').css('display', 'block');
+                $('.cancel').css('display', 'none');
+                return;
+            }
+            for(let name of this.searchClassify){
+                if(name===this.selected.folderName){
+                    Vue.set(this.messagebox, 'msg', "您已有该分组名");
+                    $('.create-chat-folder').css('display', 'block');
+                    $('.cancel').css('display', 'none');
+                    return;
+                }
+            }
+            if(this.addOrEdit){
+                Vue.set(this.messagebox, 'option', 2);
+                Vue.set(this.messagebox, 'msg', "您确定添加此联系人分组？");
+                $('.create-chat-folder').css('display', 'block');
+            }
+            else if(!this.addOrEdit){
+                $('.create-chat-folder').css('display', 'block');
+            }
+        },
+        frameClick(index,e){
+            if(index===this.frame.frameActive||this.frame.frameActive<0) return;
+
+            let left=addRippleEffect(e,e.target,"rgba(30, 144, 255, .15)");
+            let width = e.target.getBoundingClientRect().width;
+            $('.frame:eq('+index+')').css('color','dodgerblue');
+            $('.frame:eq('+this.frame.frameActive+')').css('color','gray');
+
+            //改变滑条的位置和大小
+            $('#slider').css({'width':width+'px','left':left-this.frame.first_left+'px'});
+
+            let contactList=document.getElementsByClassName('contact-list');
+            contactList[this.frame.frameActive].style.width="0";
+            contactList[this.frame.frameActive].style.opacity="0";
+            contactList[index].style.width="100%";
+            contactList[index].style.opacity="1";
+
+            contactList[index].style.display="block";
+            contactList[this.frame.frameActive].style.display="none";
+            this.frame.frameActive=index;
+        },
+        contactClick(index,e){
+            if(this.contactSelect.frameActive===this.frame.frameActive&&this.contactSelect.contactActive===index) return;
+            if(this.contactSelect.contactActive>=0){
+                $('.contact-list:eq('+this.contactSelect.frameActive+') '+'.contact-items:eq('+this.contactSelect.contactActive+')').removeClass('active');
+            }
+            this.contactSelect.contactActive=index;
+            this.contactSelect.frameActive=this.frame.frameActive;
+            this.currentChat.contactInfo=this.contact[this.contactSelect.frameActive][index];
+            this.sendInfo.toUser=this.currentChat.contactInfo.contactid;
+            e.target.classList.add('active');
+            //打开信息栏
+            $('.right-close:eq(0)').css('transform','');
+            $('.ordinary:eq(0)').css({'width':'100%','opacity':'1'});
+            $('.right-info:eq(0)').css({'width':'100%','opacity':'1'});
+            //隐藏提示词
+            $('.reminder:eq(0)').css('display','none');
+            //显示聊天主界面
+            $('.top:eq(0)').css('height','3.5rem');
+            //显示聊天输入框
+            $('.input-frame:eq(0)').css('height','4rem');
+            onLoadChatMain();
+            if(statisticsActive!==-1){
+                $('.statistics-close-icon:eq(statisticsActive)').click();
+            }
+        },
+        doNotDisturb(){
+            this.currentChat.contactInfo.doNotDisturb=!this.currentChat.contactInfo.doNotDisturb;
+            //查找所有修改bell
+        },
+        sendMsg(){
+            send(this.sendInfo.fromUser,this.sendInfo.toUser,this.sendInfo.content);
+            this.sendInfo.content="";
+        },
+        putMessage(message){
+            let JsonObject=JSON.parse(message);
+            let row=vue.contactSelect.frameActive,col=vue.contactSelect.contactActive;
+            JsonObject.time=formatDate(JsonObject.time);
+            this.contact[row][col].chatInfo.push(JsonObject);
         },
 
-    }
+    },
+    components: {
+    },
 });
 
 function getCookie(name) {
@@ -137,19 +309,29 @@ function getCookie(name) {
     //遍历匹配
     for (let i = 0; i < arrcookie.length; i++) {
         let arr = arrcookie[i].split("=");
-        if (arr[0] == name) {
+        if (arr[0] === name) {
             return arr[1];
         }
     }
     return "";
 }
 
-function Contact(contactId, headPortrait, unRead,nickname,doNotDisturb) {
+function Contact(contactId, headPortrait,nickname,doNotDisturb,phone,index,chatInfo) {
     this.contactid=contactId;
     this.headportrait=headPortrait;
-    this.unread=unRead;
     this.nickname=nickname;
     this.doNotDisturb=doNotDisturb;
+    this.phone=phone;
+    if(index===0){
+        this.select=false;
+    }
+    this.chatInfo=[];
+    if(chatInfo!==null&&chatInfo!==undefined){
+        for(let item of chatInfo){
+            item.time=formatDate(item.time);
+            this.chatInfo.push(item);
+        }
+    }
 }
 //向服务器请求用户信息（除密码）
 (function () {
@@ -163,6 +345,8 @@ function Contact(contactId, headPortrait, unRead,nickname,doNotDisturb) {
         vue.userInfo.gender = userData.gender;
         vue.userInfo.email = userData.email;
         vue.userInfo.phone = userData.phone;
+        vue.userInfo.id = userData.id;
+        vue.userInfo.headportrait=(msg.data.obj.headportrait===null)?'./image/1.jpeg':'/headportrait/'+msg.data.obj.headportrait;
 
         vue.contactClassify = msg.data.obj.folder;
 
@@ -176,7 +360,7 @@ function Contact(contactId, headPortrait, unRead,nickname,doNotDisturb) {
             let person=msg.data.obj.contact[p];
             let index=0;
             for(let name of vue.contactClassify){
-                if(person.folder==name)
+                if(person.folder===name)
                     break;
                 index++;
             }
@@ -190,9 +374,15 @@ function Contact(contactId, headPortrait, unRead,nickname,doNotDisturb) {
             vue.contact[index].push(new Contact(
                 person.contactid,
                 img,
-                person.unread,
                 person.nickname,
-                person.doNotDisturb));
+                person.doNotDisturb,
+                person.phone,index,
+                person.chatInfoList));
         }
+
+        //改变滑条的位置和大小
+        $('#slider').css('width','77.5px');
+
+        vue.sendInfo.fromUser=getCookie('id');
     });
 })();
